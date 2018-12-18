48737ab88f8929824924321faf6620056d7747ac
İlyas Doğruer <ilyasdogruer@gmail.com>
2018-12-18 14:52:05 +0300

    New class methods added to NSString+Extensions category

--- Ophelia/Categories/NSArray+Extensions.h
+++ Ophelia/Categories/NSArray+Extensions.h
@@ -1,9 +1,12 @@
 //
-//  NSArray+Extensions.h
+//  NSArray+Extensions
 //  Ophelia
 //
 //  Created by İlyas Doğruer on 18.12.2018.
 //  Copyright © 2018 Kartega. All rights reserved.
+//
+//  For the full copyright and license information, please view the LICENSE
+//  file that was distributed with this source code.
 //
 
 #import <Foundation/Foundation.h>
@@ -12,9 +15,39 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface NSArray (Extensions)
 
-+(NSArray *)sortedArrayFromSourceArray:(NSArray *)sourceArray sortBy:(NSString *)key ascending:(BOOL)ascending;
+/**
+ Sort the array by the given sort key and sort direction
+ 
+ @param sourceArray   The array object to sort
+ @param sortKey       Object attribute name
+ @param ascending     Sort direction ASC or DESC
+ */
++(NSArray *)sortedArrayFromSourceArray:(NSArray *)sourceArray sortKey:(NSString *)sortKey ascending:(BOOL)ascending;
 
+
+/**
+ Filter the array with the given predicate object
+ 
+ @param sourceArray   The array object to filter
+ @param predicate     NSPredicate for filter
+ */
 +(NSArray *)filteredArrayFromSourceArray:(NSArray *)sourceArray filterWith:(NSPredicate *)predicate;
+
+
+/**
+ Group the array with the given key
+ 
+ @param sourceArray   The array object to group
+ @param groupKey      The key to group array by
+ @param ascending     Sort direction ASC or DESC
+ */
++(NSArray *)groupedArray:(NSArray *)sourceArray groupKey:(NSString *)groupKey ascending:(BOOL)ascending;
+
+
+/**
+ Randomize the array
+ */
+-(NSArray *)randomizeArray;
 
 @end
 
--- Ophelia/Categories/NSArray+Extensions.m
+++ Ophelia/Categories/NSArray+Extensions.m
@@ -1,17 +1,20 @@
 //
-//  NSArray+Extensions.m
+//  NSArray+Extensions
 //  Ophelia
 //
 //  Created by İlyas Doğruer on 18.12.2018.
 //  Copyright © 2018 Kartega. All rights reserved.
+//
+//  For the full copyright and license information, please view the LICENSE
+//  file that was distributed with this source code.
 //
 
 #import "NSArray+Extensions.h"
 
 @implementation NSArray (Extensions)
 
-+(NSArray *)sortedArrayFromSourceArray:(NSArray *)sourceArray sortBy:(NSString *)key ascending:(BOOL)ascending {
-    NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:key ascending:ascending];
++(NSArray *)sortedArrayFromSourceArray:(NSArray *)sourceArray sortKey:(NSString *)sortKey ascending:(BOOL)ascending {
+    NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:sortKey ascending:ascending];
     NSArray *sortDescriptors = [NSArray arrayWithObject:sortDescriptor];
     NSArray *sortedArray = [sourceArray sortedArrayUsingDescriptors:sortDescriptors];
     return sortedArray;
@@ -22,4 +25,42 @@
     return filteredArray;
 }
 
++(NSArray *)groupedArray:(NSArray *)sourceArray groupKey:(NSString *)groupKey ascending:(BOOL)ascending {
+    NSMutableDictionary *dictionary = [NSMutableDictionary dictionary];
+    
+    for (id obj in sourceArray) {
+        id keyValue = [obj valueForKey:groupKey];
+        if (keyValue) {
+            NSMutableArray *array = dictionary[keyValue];
+            if (!array) {
+                array = [NSMutableArray array];
+                dictionary[keyValue] = array;
+            }
+            [array addObject:obj];
+        }
+    }
+    
+    NSMutableArray *finalArray = [NSMutableArray new];
+    for (NSString *key in [dictionary allKeys]) {
+        NSMutableDictionary *dic = [NSMutableDictionary new];
+        [dic setObject:[dictionary objectForKey:key] forKey:@"data"];
+        [dic setObject:key forKey:@"title"];
+        [finalArray addObject:dic];
+    }
+    
+    return [NSArray sortedArrayFromSourceArray:finalArray sortKey:@"title" ascending:ascending];
+}
+
+-(NSArray *)randomizeArray {
+    NSMutableArray *mutableArray = [NSMutableArray arrayWithArray:self];
+    NSUInteger count = [mutableArray count];
+    if (count > 1) {
+        for (NSUInteger i = count - 1; i > 0; --i) {
+            [mutableArray exchangeObjectAtIndex:i withObjectAtIndex:arc4random_uniform((int32_t)(i + 1))];
+        }
+    }
+    return [NSArray arrayWithArray:mutableArray];
+}
+
 @end
+
