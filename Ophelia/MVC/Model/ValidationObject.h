5cd312706f86bc63be3896ec1c67dfd9f80de884
İlyas Doğruer <ilyasdogruer@gmail.com>
2018-12-19 15:24:31 +0300

    Models added.

---
+++ Ophelia/MVC/Model/ValidationObject.h
@@ -0,0 +1,20 @@
+//
+//  ValidationObject
+//  Ophelia
+//
+//  Created by İlyas Doğruer on 19.12.2018.
+//  Copyright © 2018 Kartega. All rights reserved.
+//
+//  For the full copyright and license information, please view the LICENSE
+//  file that was distributed with this source code.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ValidationObject : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
