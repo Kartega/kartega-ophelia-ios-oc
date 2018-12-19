35dead880c8389245cd24e2ea3abda732d1bc248
İlyas Doğruer <ilyasdogruer@gmail.com>
2018-12-19 08:34:59 +0300

    Framework public header file added.

--- Ophelia/Ophelia.h
+++ Ophelia/Ophelia.h
@@ -5,15 +5,32 @@
 //  Created by İlyas Doğruer on 18.12.2018.
 //  Copyright © 2018 Kartega. All rights reserved.
 //
+//  For the full copyright and license information, please view the LICENSE
+//  file that was distributed with this source code.
+//
 
 #import <UIKit/UIKit.h>
 
-//! Project version number for Ophelia.
 FOUNDATION_EXPORT double OpheliaVersionNumber;
-
-//! Project version string for Ophelia.
 FOUNDATION_EXPORT const unsigned char OpheliaVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <Ophelia/PublicHeader.h>
+//CATEGORIES
+#import <Ophelia/NSArray+Extensions.h>
+#import <Ophelia/NSDate+Extensions.h>
+#import <Ophelia/NSString+Extensions.h>
+#import <Ophelia/UIButton+Extensions.h>
+#import <Ophelia/UIColor+Extensions.h>
+#import <Ophelia/UIImage+Extensions.h>
+#import <Ophelia/UILabel+Extensions.h>
+#import <Ophelia/UIView+Extensions.h>
+
+
+//VIEWS
+
+
+//CONTROLLER
+
+
+//UTILITIES
 
 
