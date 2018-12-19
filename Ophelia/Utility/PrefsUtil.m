0cfed5669ff06a7e601befc4190ce584067b56b4
İlyas Doğruer <ilyasdogruer@gmail.com>
2018-12-19 15:24:00 +0300

    Utility classes added.

---
+++ Ophelia/Utility/PrefsUtil.m
@@ -0,0 +1,16 @@
+//
+//  PrefsUtil
+//  Ophelia
+//
+//  Created by İlyas Doğruer on 19.12.2018.
+//  Copyright © 2018 Kartega. All rights reserved.
+//
+//  For the full copyright and license information, please view the LICENSE
+//  file that was distributed with this source code.
+//
+
+#import "PrefsUtil.h"
+
+@implementation PrefsUtil
+
+@end
---
+++ Ophelia/Utility/ShareUtil.m
@@ -0,0 +1,16 @@
+//
+//  ShareUtil
+//  Ophelia
+//
+//  Created by İlyas Doğruer on 19.12.2018.
+//  Copyright © 2018 Kartega. All rights reserved.
+//
+//  For the full copyright and license information, please view the LICENSE
+//  file that was distributed with this source code.
+//
+
+#import "ShareUtil.h"
+
+@implementation ShareUtil
+
+@end
